cca708e37d46d61d140f6df4df043b1be7dd2754
Temi Bankole <temibankole5@gmail.com>
2025-10-15 09:18:53 -0600

    set up of merge class

--- src/utilities/MergeSort.java
+++ src/utilities/MergeSort.java
@@ -1,4 +1,18 @@
 package utilities;
+import shapes.Shape;
+import java.util.*;
+
+/**
+ * Utilities.java
+ *
+ * @author temi
+ * @version 4.36
+ * 
+ * Class Definition: This class implements two different Merge methods one using compareTo() and one using compare()
+ * The implementation works by recursively diving the input array into two halves, then sorts the two halves,  
+ * and then merges them back together to obtain the sorted array. It divide, conquers, and merges.
+ * All methods are required in order to the merge functionality.
+ */
 
 public class MergeSort
 {
