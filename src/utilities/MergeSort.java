f240213e4f977e95d088b4fcbb5f1d55d7681525
Temi Bankole <temibankole5@gmail.com>
2025-10-16 10:54:34 -0600

    formatting of the for loops in the mergesort methods

--- src/utilities/MergeSort.java
+++ src/utilities/MergeSort.java
@@ -29,9 +29,13 @@ public class MergeSort
 		
 		//Copy data into to temporary arrays
 		for (int i = 0; i < n1; ++i)
+		{
 			Left[i] = array[left + i];
+		}
 		for (int j = 0; j < n2; ++j)
+		{
 			Right[j] = array[mid + 1 + j];
+		}
 		
 		//Merge the temporary arrays
 		int i = 0, j = 0, k = left;
@@ -89,9 +93,13 @@ public class MergeSort
 			
 		//Copy data into to temporary arrays
 		for (int i = 0; i < n1; ++i)
+		{
 			Left[i] = array[left + i];
+		}
 		for (int j = 0; j < n2; ++j)
+		{
 			Right[j] = array[mid + 1 + j];
+		}
 			
 		//Merge the temporary arrays
 		int i = 0, j = 0, k = left;
